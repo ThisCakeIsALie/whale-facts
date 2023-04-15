111298ab32455533bcf9e74946d21c4e850c3a7d
ThisCakeIsALie <patrick01999@gmail.com>
2023-04-15 14:21:02 +0200

    Finishing up the website

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const factText = document.getElementById("fact-text");
-const factButton = document.getElementById("fact-button");
+async function main() {
+    const factText = document.getElementById("fact-text");
+    const body = document.querySelector("body");
 
-factButton.addEventListener("click", () => {
-    fetch("whale_facts.json")
-        .then(response => response.json())
-        .then(data => {
-            const randomFact = data[Math.floor(Math.random() * data.length)];
-            factText.innerText = randomFact;
-        })
-        .catch(error => {
-            console.error("Error fetching whale facts:", error);
-            factText.innerText = "An error occurred while fetching whale facts. Please try again.";
-        });
-});
+    const WHALE_FACTS = await fetch("whale_facts.json").then(response => response.json());
+
+    async function showRandomFact() {
+        factText.style.opacity = 0;
+
+        // Wait for the opacity to transition to 0
+        await new Promise(resolve => setTimeout(resolve, 500));
+
+        factText.style.opacity = 1;
+        factText.innerText = WHALE_FACTS[Math.floor(Math.random() * WHALE_FACTS.length)];
+    }
+
+    // When clicked, show a random whale fact
+    body.addEventListener("click", async (event) => {
+        if (event.target !== factText && event.target.id !== "click-info") {
+            await showRandomFact();
+        }
+    });
+
+    // When the page loads, show a random whale fact
+    factText.innerText = WHALE_FACTS[Math.floor(Math.random() * WHALE_FACTS.length)];
+}
+
+main();
